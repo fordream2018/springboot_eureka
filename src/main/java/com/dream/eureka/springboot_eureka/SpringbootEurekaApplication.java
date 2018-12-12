7cb5911005dcb5ce01c36b91a54ed3f49dd7b08f
suorongbo <suorongbo@vip.qq.com>
2018-12-13 00:35:29 +0800

    eureka

---
+++ src/main/java/com/dream/eureka/springboot_eureka/SpringbootEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.dream.eureka.springboot_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootEurekaApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbootEurekaApplication.class, args);
+    }
+    
+}
