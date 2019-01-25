2afda970b57420ec6d3be9dc457b096efeb8cc5f
fordreram2018 <fordream2018@126.com>
2019-01-25 23:18:46 +0800

    [desc]:boot [moudle]:boot [author]:lifei [data]:20190125

--- src/main/java/com/dream/eureka/springboot_eureka/SpringbootEurekaApplication.java
+++ src/main/java/com/dream/eureka/springboot_eureka/SpringbootEurekaApplication.java
@@ -2,9 +2,11 @@ package com.dream.eureka.springboot_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootEurekaApplication
 {
